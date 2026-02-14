f520d1d7867341d7f263ee1fb5441578b1dea65e
Josh Walker <walkerje1992@gmail.com>
2026-02-14 14:55:24 -0700

    add main test for loading ROM and dumping memory state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod chip8;
+
+use std::env;
+
+use crate::chip8::Chip8;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("INCORRECT USE!! Usage: ");
+        print!("cargo run <path_to_rom_file>");
+
+        return;
+    }
+
+    let mut emulated_chip8 = Chip8::new();
+
+    emulated_chip8.load_instructions(&args[1]);
+
+    emulated_chip8.dump_memory_state();
 }
